be9fea42d894fbfa3cb51a48fb64da62b9fcc918
sarahmarkland <sarah.markland@icloud.com>
2023-12-06 14:24:01 -0600

    task 2 aaahhhhh

--- scripts.js
+++ scripts.js
@@ -64,56 +64,65 @@ function showLoader() {
    }
 
    function fetchTutorials() {
-    // Display loader before making the request
-    showLoader();
-   
-    // Ajax request to get quotes from the API
-    $.ajax({
-     url: "https://smileschool-api.hbtn.info/popular-tutorials",
-     method: "GET",
-     dataType: "json",
-     success: function (data) {
-       // Handle successful response
-       if (data && data.length > 0) {
-         // Clear existing quotes in the carousel
-         const popularCards = document.querySelector(".popular-cards");
-          popularCards.innerHTML = "";
-         // Iterate through the cards and add them to the carousel
-         data.forEach(function () {
-           var cardItem = `<div class="carousel">
-                            <div class="quote-item">
-                              <img src="${quote.pic_url}" class="quote-image">
-                              <blockquote class="blockquote">
-                                <p class="quote-text">${quote.text}</p>
-                                <h4 class="quote-text">${quote.name}</h4>
-                                <span class="quote-text">${quote.title}</span>
-                              </blockquote>
-                            </div>
-                          </div>`;
-   
-           carouselInner.innerHTML += quoteItem;
-         });
-   
-         // Activate first item in the carousel
-         const carouselItems = document.querySelectorAll(".carousel-item");
-         carouselItems[0].classList.add("active");
-       } else {
-         // Handle the case where no quotes are received
-         console.error("No quotes received from the API");
-       }
-   
-       // Hide loader after quotes are loaded
-       hideLoader();
-     },
-     error: function (error) {
-       // Handle Ajax request error
-       console.error("Error fetching quotes:", error);
-   
-       // Hide loader in case of an error
-       hideLoader();
-     },
-    });
+    fetch('https://smileschool-api.hbtn.info/popular-tutorials')
+      .then(response => response.json())
+      .then(data => {
+        // Process data and initialize the carousel
+        initializeCarousel(data);
+      })
+      .catch(error => console.error('Error fetching data:', error));
    }
+
+   function initializeCarousel(data) {
+    data.forEach(item => {
+      $('#popular-card').append(`
+              <div class="card p-3" >
+                <img src="${item.thumb_url}" alt="${item.title}" class="card-img-top">
+                <div class="card-img-overlay" style="top: 25px; top: 5%">
+                  <img src="images/play.png" alt="Play" width="64px" class="align-self-center play-overlay" />
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title font-weight-bold">${item.title}</h5>
+                  <p class="card-text text-muted">${item['sub-title']}</p>
+                  <div class="creator d-flex align-items-center">
+                    <img src="${item.author_pic_url}" alt="${item.author}" class="rounded-circle" width="30px">
+                  </div>
+                  <h6 class="pl-3 m-0 main-color">${item.author}</h6>
+                </div>
+                <div class="info pt-3 d-flex justify-content-between">
+                  <div class="rating">
+                    <img src="images/star_on.png" alt="star" width="15px">
+                    <span class="pl-2 pr-2">${item.star}</span>
+                  </div>
+                  <div class="duration">
+                    <span>${item.duration}</span>
+                  </div>
+                </div>
+              </div>
+      `);
+    });    
+    console.log("data populated");
+    $('#popular-card').slick({
+      slidesToShow: 4, // Adjust based on window size
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+          }
+        }
+      ]
+    });
+    console.log("carousel initialized");
+    // Populate carousel with data
+  }
    
    
 
--- slick-scripts.js
+++ slick-scripts.js
@@ -11,76 +11,58 @@ fetch('https://smileschool-api.hbtn.info/popular-tutorials')
 
 
   function initializeCarousel(data) {
+    populateCarousel(data);
+    console.log("data populated");
     $('#popular-card').slick({
-      dots: true,
-      infinite: true,
-      speed: 300,
-      slidesToShow: calculateSlidesToShow(), // Adjust based on window size
+      slidesToShow: 4, // Adjust based on window size
       slidesToScroll: 1,
       responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 1,
-          },
-        },
         {
           breakpoint: 768,
           settings: {
             slidesToShow: 2,
-            slidesToScroll: 1,
           },
         },
         {
-          breakpoint: 480,
+          breakpoint: 576,
           settings: {
             slidesToShow: 1,
-            slidesToScroll: 1,
-          },
-        },
-      ],
+          }
+        }
+      ]
     });
-  
+    console.log("carousel initialized");
     // Populate carousel with data
-    populateCarousel(data);
   }
 
   function populateCarousel(data) {
+    console.log("populating data");
     data.forEach(item => {
-      $('#popular-card').slick('slickAdd', `
-        <div class="carousel slide">
-          <div class="card">
-          <img src="${item.thumb_url}" alt="${item.title}">
-          <h3>${item.title}</h3>
-          <p>${item['sub-title']}</p>
-          <div class="carousel-card-info">
-            <div class="carousel-card-info-left">
-              <img src="${item.author_pic_url}" alt="${item.author}">
-              <p>${item.author}</p>
-            </div>
-            <div class="carousel-card-info-right">
-              <img src="images/star_on.png" alt="star">
-              <p>${item.star}</p>
-            </div>
-          </div>
+      $('#popular-card').append(`
+              <div class="card p-3" >
+                <img src="${item.thumb_url}" alt="${item.title}" class="card-img-top">
+                <div class="card-img-overlay" style="top: 25px; top: 5%">
+                  <img src="images/play.png" alt="Play" width="64px" class="align-self-center play-overlay" />
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title font-weight-bold">${item.title}</h5>
+                  <p class="card-text text-muted">${item['sub-title']}</p>
+                  <div class="creator d-flex align-items-center">
+                    <img src="${item.author_pic_url}" alt="${item.author}" class="rounded-circle" width="30px">
+                  </div>
+                  <h6 class="pl-3 m-0 main-color">${item.author}</h6>
+                </div>
+                <div class="info pt-3 d-flex justify-content-between">
+                  <div class="rating">
+                    <img src="images/star_on.png" alt="star" width="15px">
+                    <span class="pl-2 pr-2">${item.star}</span>
+                  </div>
+                  <div class="duration">
+                    <span>${item.duration}</span>
+                  </div>
+                </div>
+              </div>
       `);
     });
   }
-  
-  function calculateSlidesToShow() {
-    const windowWidth = $(window).width();
-    if (windowWidth >= 1024) {
-      return 4;
-    } else if (windowWidth >= 768) {
-      return 2;
-    } else {
-      return 1;
-    }
-  }
-  
-  $(window).on('resize', () => {
-    const slidesToShow = calculateSlidesToShow();
-    $('#carousel').slick('slickSetOption', 'slidesToShow', slidesToShow, true);
-  });
 });
