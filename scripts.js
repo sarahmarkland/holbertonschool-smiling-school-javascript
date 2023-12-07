a74039b0d625712b4e0dc5248bfeb286dc9db764
sarahmarkland <sarah.markland@icloud.com>
2023-12-07 10:44:22 -0600

    task 3 Latest Videos start

--- scripts.js
+++ scripts.js
@@ -79,20 +79,19 @@ function showLoader() {
               <div class="card p-3" >
                 <img src="${item.thumb_url}" alt="${item.title}" class="card-img-top">
                 <div class="card-img-overlay" style="top: 25px; top: 5%">
-                  <img src="images/play.png" alt="Play" width="64px" class="align-self-center play-overlay" />
+                  <img src="images/play.png" alt="Play" width="64px" class="align-self-center play-overlay m-auto" />
                 </div>
                 <div class="card-body">
                   <h5 class="card-title font-weight-bold">${item.title}</h5>
                   <p class="card-text text-muted">${item['sub-title']}</p>
                   <div class="creator d-flex align-items-center">
                     <img src="${item.author_pic_url}" alt="${item.author}" class="rounded-circle" width="30px">
+                    <h6 class="pl-3 m-0 main-color">${item.author}</h6>
                   </div>
-                  <h6 class="pl-3 m-0 main-color">${item.author}</h6>
                 </div>
                 <div class="info pt-3 d-flex justify-content-between">
                   <div class="rating">
-                    <img src="images/star_on.png" alt="star" width="15px">
-                    <span class="pl-2 pr-2">${item.star}</span>
+                    <img class="" src="./images/rating.png">
                   </div>
                   <div class="duration">
                     <span>${item.duration}</span>
@@ -103,8 +102,10 @@ function showLoader() {
     });    
     console.log("data populated");
     $('#popular-card').slick({
-      slidesToShow: 4, // Adjust based on window size
+      slidesToShow: 3.99, // Adjust based on window size
       slidesToScroll: 1,
+      prevArrow: $(".prev1"),
+      nextArrow: $(".next1"),
       responsive: [
         {
           breakpoint: 768,
@@ -123,8 +124,6 @@ function showLoader() {
     console.log("carousel initialized");
     // Populate carousel with data
   }
-   
-   
 
   // Call the fetchQuotes function when the document is ready
   fetchQuotes();
